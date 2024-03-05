34ae43bb5ea3d112ec87770c5b80c8c940488c25
shiyi <643876393@qq.com>
2024-03-05 22:01:40 +0800

    Initial commit 第一次

---
+++ src/yhr.java
@@ -0,0 +1,9 @@
+public class yhr {
+//叫做main方法。表示程序的入口
+    public static void main(String[] args) {
+        /*叫做输出语句（打印语句）
+         会把小括号里面的内容打印出来*/
+        System.out.println("131499");
+    }
+
+    }
---
+++ src/yhra/yhr/shiyan/passwd.java
@@ -0,0 +1,7 @@
+package yhra.yhr.shiyan;
+
+public class passwd {
+    public static void main(String[] args) {
+        System.out.println("麻瓜");
+    }
+}
