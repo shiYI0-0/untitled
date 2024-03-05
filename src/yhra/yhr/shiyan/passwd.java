a6861b422f82f53dbe366cc225ddd55f3aa160f5
shiyi <643876393@qq.com>
2024-03-05 22:16:53 +0800

    第一次文件修改增加随机数抽取10个

--- src/yhra/yhr/shiyan/passwd.java
+++ src/yhra/yhr/shiyan/passwd.java
@@ -1,7 +1,17 @@
 package yhra.yhr.shiyan;
 
+import java.util.Random;
+
 public class passwd {
     public static void main(String[] args) {
         System.out.println("麻瓜");
+        Random r=new Random();
+        for (int i = 0; i < 10; i++) {
+            int aa=r.nextInt(99)+1;
+            System.out.println(aa);
+        }
+
+
+        System.out.println("aaa");
     }
 }
